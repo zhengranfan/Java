ce4bd373e9f22d50baea6cb6b814bf4999d2f8fa
ia <ia@192.168.0.105>
2019-07-10 21:35:11 +0800

    实现userdao接口

--- src/com/fan/jdcb/dao/impl/UseDaoImpl.java
+++ src/com/fan/jdcb/dao/impl/UseDaoImpl.java
@@ -1,7 +1,46 @@
 package com.fan.jdcb.dao.impl;
 
 import com.fan.jdcb.dao.UseDao;
+import com.fan.jdcb.domain.User;
 
 public class UseDaoImpl  implements UseDao{
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	//	Class.forName(className)
+		
+	}
+
+	@Override
+	public User getUser(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findUser(String loginName, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+	
+		
+		
+		
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+	
+		
+		
+		
+	}
 
 }
